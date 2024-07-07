9a85b203a7feb284ebe9db33d1cf0a49c3c08d15
kimi.casamina <kimi.casamina@gmail.com>
2024-07-07 12:43:47 +0800

    modified axios baseURL

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 // axios imports and config
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8080/";
+axios.defaults.baseURL = "https://rssreader.onrender.com/";
 axios.defaults.withCredentials = true;
 
 // redux
